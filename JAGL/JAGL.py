53ba84921af9d376f053c0909194089a84feaacb
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-10-27 01:20:40 +0300

    Update JAGL.py

--- JAGL/JAGL.py
+++ JAGL/JAGL.py
@@ -1,10 +1,12 @@
-i="""@Hello, World!
-#""";p=0;s=[];ts=[]
+i="""@Hello, World!|#""";p=0;s=[];ts=[]
 while p!=len(i):
  if i[p]=="@":
   p+=1
-  while i[p]!="\n":ts.append(i[p]);p+=1
+  while 1:
+   ts.append(i[p]);p+=1
+   if i[p]=="\n" or i[p]=="|":break
   while len(ts)>1:a=str(ts[0])+str(ts[1]);ts.pop(0);ts.pop(0);ts.reverse();ts.append(a);ts.reverse()
   s.append(ts[0]);ts.pop
  if i[p]=="#":s.reverse();print(s[0]);s.reverse();s.pop()
+ if i[p]=="ƒ":s.reverse();print(s[0],end="");s.reverse();s.pop()
  p+=1
