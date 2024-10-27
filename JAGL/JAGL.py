0204ed8ba526c0f27246a7fb0174bd517a1bd07f
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-10-27 23:33:58 +0300

    Update JAGL.py

--- JAGL/JAGL.py
+++ JAGL/JAGL.py
@@ -1,11 +1,11 @@
-i="""@helo!|#"""
+i="""@hi|#~"""
 p=0;s=[];ts=[];m=0
 while True:
  if i[p]=="@":
   p+=1
   while 1:
    ts.append(i[p]);p+=1
-   if i[p]=="\n" or i[p]=="|":break
+   if i[p]=="|":break
   while len(ts)>1:a=str(ts[0])+str(ts[1]);ts.pop(0);ts.pop(0);ts.reverse();ts.append(a);ts.reverse()
   s.append(ts[0]);ts.pop
  if i[p]=="#":s.reverse();print(s[0]);s.reverse();s.pop()
