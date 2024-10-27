0a43943d677bf5c18903b7aa437c0544f99584ca
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-10-27 23:32:24 +0300

    Update JAGL.py

--- JAGL/JAGL.py
+++ JAGL/JAGL.py
@@ -1,14 +1,20 @@
-i="""@Hello, World!|#@heh|#""";p=0;s=[];ts=[]
-while p!=len(i):
+="""@helo!|#"""
+p=0;s=[];ts=[];m=0
+while True:
  if i[p]=="@":
   p+=1
   while 1:
    ts.append(i[p]);p+=1
    if i[p]=="\n" or i[p]=="|":break
   while len(ts)>1:a=str(ts[0])+str(ts[1]);ts.pop(0);ts.pop(0);ts.reverse();ts.append(a);ts.reverse()
-  s.append(ts[0]);ts.pop()
- if i[p]=="#":
-  if len(s)>0:s.reverse();print(s[0]);s.reverse();s.pop()
- if i[p]=="ƒ":
-  if len(s)>0:s.reverse();print(s[0],end="");s.reverse();s.pop()
+  s.append(ts[0]);ts.pop
+ if i[p]=="#":s.reverse();print(s[0]);s.reverse();s.pop()
+ elif i[p]=="ƒ":s.reverse();print(s[0],end="");s.reverse();s.pop()
+ elif i[p]=="~":m=1
  p+=1
+ if p==len(i) and m==0:break
+ elif p==len(i) and m==1:
+  while True:
+   p-=1
+   if i[p]=="\n":break
+   elif p==0:break
