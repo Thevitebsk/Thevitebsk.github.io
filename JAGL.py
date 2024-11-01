c956a65edffaf38f78b133923c074d0ef7b3c04b
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-11-01 22:22:15 +0300

    Moved this to the main directory

---
+++ JAGL.py
@@ -0,0 +1,20 @@
+i="""@hi|#"""
+p=0;s=[];ts=[];m=0
+while 1:
+ if i[p]=="@":
+  p+=1
+  while 1:
+   ts.append(i[p]);p+=1
+   if i[p]=="|":break
+  while len(ts)>1:a=str(ts[0])+str(ts[1]);ts.pop(0);ts.pop(0);ts.reverse();ts.append(a);ts.reverse()
+  s.append(ts[0]);ts.pop
+ if i[p]=="#":s.reverse();print(s[0]);s.reverse();s.pop()
+ elif i[p]=="ƒ":s.reverse();print(s[0],end="");s.reverse();s.pop()
+ elif i[p]=="~":m=1
+ p+=1
+ if p==len(i) and m==0:break
+ elif p==len(i) and m==1:
+  while 1:
+   p-=1
+   if i[p]=="\n":m=0;break
+   elif p==0:m=0;break
