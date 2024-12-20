f817934726d458b4af1e2a83ba2b4eac80a62958
Gaham <glebsemenov1mail.ru@gmail.com>
2024-12-20 22:05:55 +0300

    Update func.js

--- jive/func.js
+++ jive/func.js
@@ -1,4 +1,6 @@
 function r(start, end) {
   return Array.from({ length: end - start }, (_, i) => start + i)
 }
-function fact(n){alert("will work soon")}
+function fact(n){
+  //alert. Will be worked on soon
+}
