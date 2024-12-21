080ea70d95593d7fcdf3c3ffcc3082f4a5ae2cc2
Gaham <glebsemenov1mail.ru@gmail.com>
2024-12-21 11:27:24 +0300

    Update jive.py

--- jive/jive.py
+++ jive/jive.py
@@ -1,5 +1,4 @@
-p=0;tv=0;tv2=0;s=[];num=list(map(str,range(10)))
-c=""""""
+p=0;tv=0;tv2=0;s=[];c=""""""
 while len(c)>p:
  if c[p]=="*":s.append(input())
  elif c[p]=="`":
@@ -7,12 +6,11 @@ while len(c)>p:
   elif c[p+1]==".":s[-1]=float(s[-1])
   elif c[p+1]=="\"":s[-1]=str(s[-1])
  elif c[p]=="!":
-  tv2=tv=s[-1]
+  tv2=tv=s.pop()
   while tv2>1:tv2-=1;tv*=tv2
   s.append(tv)
  elif c[p]=="↑":print(s)
  elif c[p]=="↓":print(s.pop())
  elif c[p]=="[":
   while c[p]!="]":p+=1
- elif c[p]in num:s.append(int(c[p]))
  p+=1
