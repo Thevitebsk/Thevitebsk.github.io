dfe9dff6ebd343233dbef7962264b3cce1f91c16
Gaham <glebsemenov1mail.ru@gmail.com>
2024-12-21 10:01:52 +0300

    Update func.js

--- jive/func.js
+++ jive/func.js
@@ -1,7 +1,8 @@
 function r(n, k) {
   return Array.from({length:k-n}, (_,i)=>n+i)
 }
-function fact(n){
-  t2 = t = n ; while (t2 > 1){t2 -= 1;t *= t2}
-  return t2
+function fact(n) {
+  let t = 1;
+  for (let t2 = n; t2 > 1; t2--) { t *= t2 }
+  return t;
 }
