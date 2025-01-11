f1e3c49c5959b44ab2d87dd1250821b8b3bb31e8
Gaham <glebsemenov1mail.ru@gmail.com>
2025-01-11 23:53:31 +0300

    Update Just.py

--- Just/Just.py
+++ Just/Just.py
@@ -1,4 +1,4 @@
-i=""""""
+i="""␁#"""
 p=m=0;s=[];ts=[]
 while 1:
  if i[p]=="\"":
@@ -13,7 +13,12 @@ while 1:
  if i[p]=="#":print(s.pop())
  elif i[p]=="k":print(s[-1])
  elif i[p]=="~":m=1
- elif i[p]=="␀":...
+ elif i[p]=="␀":
+  if i[p+1]=="␅":s.append(0)
+  else:...
+ elif i[p]=="␁":
+  if i[p+1]=="␅":s.append(1)
+  else:s.append(input())
  p+=1
  if p==len(i) and m==0:break
  elif p==len(i) and m==1:
