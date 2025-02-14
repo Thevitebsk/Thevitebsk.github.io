94cc28287f62e9aa8037b60b713500f3f59f051a
Gaham <glebsemenov1mail.ru@gmail.com>
2025-02-14 11:42:50 +0300

    Update tjs.js

--- true/tjs.js
+++ true/tjs.js
@@ -1,6 +1,6 @@
 function main(){
-  p = 0 ; output = '' ; s = [] ; ts = [] //; m = 0
-  num = ["0","1","2","3","4","5","6","7","8","9"] ; code = document.getElementById("program").value ; input = document.getElementById("input").value.split("\n");
+  p = 0 ; output = '' ; s = [] ; ts = [] /*; m = 0*/ cons = output+'\nSTACK: ['+s+']\nTEMPSTACK: ['+ts+']\nBYTES: '+code.length+
+  num = ["0","1","2","3","4","5","6","7","8","9"] ; code = document.getElementById("program").value ; input = document.getElementById("input").value.split("\n") ; perma = encodeURIComponent(document.getElementById("program").value)
   while (code.length>p){
     //i will focus on the strings later
     //if (code[p]=='"'){ m=1 ; p++ };
@@ -9,6 +9,6 @@ function main(){
     else if (code[p]=="."){ if (s.length>0){ output += s.pop() } else { break } }
     else if (code[p]=="-"){if (s.length>0){ s.pop()} else { break } }
     else if (code[p]=="\n"){ break }
-    p++
-  } ; document.getElementById("console").innerHTML=output+'\nSTACK: ['+s+']\nTEMPSTACK: ['+ts+']\nBYTES: '+code.length+'PERMALINK: '+"https://thevitebsk-github-io.vercel.app/true/ti.html/?c="+encodeURIComponent(document.getElementById("program").value)
+    p++; if (perma){ cons += "\nPERMALINK: "+perma }
+  } ; document.getElementById("console").innerHTML=cons
 }
