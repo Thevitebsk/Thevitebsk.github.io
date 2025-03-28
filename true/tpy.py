0c244c97789d81f4bb90b2f77a415472c7533247
Gaham <glebsemenov1mail.ru@gmail.com>
2025-03-28 21:55:41 +0300

    Update tpy.py

--- true/tpy.py
+++ true/tpy.py
@@ -1,35 +1,37 @@
-i=''''''
+import sys
+try:code=open(sys.argv[1]).read()
+except:print(f"Usage: py {__file__.split(chr(0x5c))[-1]} \"(file name)\"");exit()
 targ="";c="";sub=tp=p=0;ts=[];s=[];ld=[]
 print("true")
 while 1:
  try:
-  if i[p]=='"':
-   while i[p+1]!='"':p+=1;ts.append(i[p])
+  if code[p]=='"':
+   while code[p+1]!='"':p+=1;ts.append(code[p])
    s.append("".join(ts));ts.clear();p+=1
-  elif i[p]=='.':print(s.pop(),end="")
-  elif i[p]in list(map(str,range(10))):s.append(int(i[p]))
-  elif i[p]==',':c=input()
-  elif i[p]==':':s.append(s[-1])
-  elif i[p]=="'":s.append(c[0]);c=c[1:]
-  elif i[p]=='»':
-   targ=i[p+1];tp=p;sub=1
+  elif code[p]=='.':print(s.pop(),end="")
+  elif code[p]in list(map(str,range(10))):s.append(int(code[p]))
+  elif code[p]==',':c=input()
+  elif code[p]==':':s.append(s[-1])
+  elif code[p]=="'":s.append(c[0]);c=c[1:]
+  elif code[p]=='»':
+   targ=code[p+1];tp=p;sub=1
    while 1:
-    if i[p]==targ and i[p+1]==":":p+=1;break
-    if len(i)-2==p:p=0
+    if code[p]==targ and code[p+1]==":":p+=1;break
+    if len(code)-2==p:p=0
     p+=1
-  elif i[p]==";"and sub==1:p=tp
-  elif i[p]=="\n":break
-  elif i[p]=="-":s.pop()
-  elif i[p]=="+":s.append(int(s.pop())+int(s.pop()))
-  elif i[p]=="`":s.append(int(s.pop())*-1)
-  elif i[p]=="(":
-   while i[p+1]!=")":ts.append(i[p+1]);p+=1
+  elif code[p]==";"and sub==1:p=tp
+  elif code[p]=="\n":break
+  elif code[p]=="-":s.pop()
+  elif code[p]=="+":s.append(int(s.pop())+int(s.pop()))
+  elif code[p]=="`":s.append(int(s.pop())*-1)
+  elif code[p]=="(":
+   while code[p+1]!=")":ts.append(code[p+1]);p+=1
    ld.append("".join(ts))
-  elif i[p]=="[":
-   while i[p]!="]":p+=1
-  if len(i)-1==p:
+  elif code[p]=="[":
+   while code[p]!="]":p+=1
+  if len(code)-1==p:
    while 1:
-    if i[p]=="\n":break
+    if code[p]=="\n":break
     elif p==0:p=-1
   p+=1
  except IndexError:break
