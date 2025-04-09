9624f66cd3727d977145ea2e4114a22b69f07b7a
Gaham <glebsemenov1mail.ru@gmail.com>
2025-04-09 23:33:51 +0300

    Update tpy.py

--- true/tpy.py
+++ true/tpy.py
@@ -1,8 +1,8 @@
 "in progress"
 import sys
-try:code=open(sys.argv[1]).read()
-except:print(f"Usage: python {__file__.split(chr(0x5c))[-1]} \"(file name)\"");exit()
-p=0;ts=[];s=[]
+try: code = open(sys.argv[1]).read()
+except: print(f"Usage: python {__file__.split(chr(0x5c))[-1]} \"(file name)\"");exit()
+p = 0;ts = [];s = []
 while 1:
   if code[p] == '"' :
     p+=1;s.append(code[p : code.index('"', p)])
@@ -12,5 +12,5 @@ while 1:
   elif code[p] in "0123456789" : s.append(int(code[p]))
   elif code[p] == "[" : p=code.index("]", p)
   if code[p] == "\n" : break
-  if len(code)==p+1:p=-1
-  p+=1
+  if len(code) == p+1:p = -1
+  p += 1
