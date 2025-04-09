615e68345cc6698b854af51cdfce84d1d9a23859
Gaham <glebsemenov1mail.ru@gmail.com>
2025-04-09 22:08:59 +0300

    Update tpy.py

--- true/tpy.py
+++ true/tpy.py
@@ -2,9 +2,12 @@
 import sys
 try:code=open(sys.argv[1]).read()
 except:print(f"Usage: python {__file__.split(chr(0x5c))[-1]} \"(file name)\"");exit()
-targ="";c="";sub=tp=p=0;ts=[];s=[];ld=[]
+p=0;ts=[];s=[]
 while 1:
- if code[p]=='"':p+=1;s.append(code[p:code.index('"',p)]);p=code.index('"',p)
- elif code[p]==".":print(s.pop())
- if code[p]=="\n":break
- p+=1
+  if code[p] == '"' :
+    p+=1;s.append(code[p:code.index('"',p)])
+    p=code.index('"',p)
+  elif code[p] == "." : print(s.pop())
+  elif code[p] == "," : s.append(input())
+  if code[p] == "\n" : break
+  p+=1
