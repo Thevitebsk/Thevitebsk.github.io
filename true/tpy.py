2d7541b2c2e7894f71396807b8b3779ba8ad6c38
Gaham <glebsemenov1mail.ru@gmail.com>
2025-04-10 11:48:52 +0300

    Update tpy.py

--- true/tpy.py
+++ true/tpy.py
@@ -1,14 +1,19 @@
 import sys
-try: code = open(sys.argv[1]).read()
+try:
+  code = open(sys.argv[1]).read()
+  try: i = open(sys.argv[2]).readlines()
+  except: i=[]
 except: print(f"Usage: python {__file__.split(chr(0x5c))[-1]} \"(file name)\"");exit()
 p = 0; ts = []; s = []
 if not code : code = " "
 while 1:
   if code[p] == '"' :
-    p+=1;s.append(code[p : code.index('"', p)])
+    p+=1; s.append(code[p : code.index('"', p)])
     p=code.index('"', p)
   elif code[p] == "." : print(s.pop())
-  elif code[p] == "," : s.append(input())
+  elif code[p] == "," : 
+    if i : s.append(i.pop())
+    else : raise EOFError("Input reached an EOF")
   elif code[p] in "0123456789" : s.append(int(code[p]))
   elif code[p] == "[" : p=code.index("]", p)
   elif code[p] == "+" : s.append(int(s.pop())+int(s.pop()))
